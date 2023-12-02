884374e7770849bcfd7b2d9e31f169cfca814450
codmmd <149286559+codmmd@users.noreply.github.com>
2023-12-02 22:36:54 +0330

    Add files via upload

---
+++ cpp1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+main() {
+	
+	float a,b;
+	
+	cout<<"please enter a number for a :";
+	cin>>a;
+	cout<<"please enter a number for b :";
+	cin>>b;
+	
+	cout<<"a+b :"<<a+b;
+	
+	cout<<"\na-b :"<<a-b;
+	
+	cout<<"\na*b :"<<a*b;
+	
+	cout<<"\na%b :"<<(int)a%(int)b;
+}
